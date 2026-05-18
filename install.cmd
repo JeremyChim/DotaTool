270a29efbfda044a41e90352ee96586a7bc143a9
JeremyChim <jer888chim@outlook.com>
2026-05-18 14:54:50 +0800

    update

--- install.cmd
+++ install.cmd
@@ -2,7 +2,13 @@ cd /d "%~dp0"
 rd /s /q "build"
 rd /s /q "dist"
 del /f /q "DotaTool.spec"
-del /f /q "DotaTool.exe"
 pyinstaller.exe -F -i main.ico main.py -n DotaTool
-copy /y "dist\DotaTool.exe" "DotaTool.exe"
+robocopy _install dist\_install /E
+robocopy bot_script dist\bot_script /E
+robocopy gi dist\gi /E
+robocopy npc dist\npc /E
+robocopy vpk dist\vpk /E
+copy config.json dist\config.json /Y
+rd /s /q "build"
+del /f /q "DotaTool.spec"
 cmd
---
+++ requirements.cmd
@@ -0,0 +1,4 @@
+cd /d "%~dp0"
+python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+cmd
