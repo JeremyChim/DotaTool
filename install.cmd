55e6412481b082ab93432aa0a97c2291fbfaaa53
JeremyChim <jer888chim@outlook.com>
2026-05-30 21:22:06 +0800

    update

--- install.cmd
+++ install.cmd
@@ -2,7 +2,7 @@ cd /d "%~dp0"
 rd /s /q "build"
 rd /s /q "dist"
 del /f /q "DotaTool.spec"
-pyinstaller.exe -F -i main.ico main.py -n DotaTool
+pyinstaller main.py
 robocopy _install dist\_install /E
 robocopy bot_script dist\bot_script /E
 robocopy gi dist\gi /E
